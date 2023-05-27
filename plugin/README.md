ed1195efb3a3ac38be93a5f620144d5b951fd94d
Chao Zhou <Czhou0k@gmail.com>
2023-05-27 17:09:25 -0400

    clone chrome extension

---
+++ plugin/README.md
@@ -0,0 +1,27 @@
+# Chrome plugin
+
+Add notes here if you want. Any discovery, issues, limitations and whatnot.
+
+## Basic Instructions from Chrome
+
+You can follow the basic instructions [here](https://developer.chrome.com/docs/extensions/mv3/getstarted/)
+
+## Requirements
+
+- Node: 16 or higher
+
+## Local Development
+
+1. Install dependencies
+
+```
+npm i
+```
+
+2. Run json-server for the database. This will run the server on default `http://localhost:3000`. You can use this to fetch data.
+
+```
+npm run db
+```
+
+**NOTE:** If you're building your own schema, please include it here
---
+++ plugin/background.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+const tabObj = {};
+const urlStatus = {
+  complete: true,
+  loading: false,
+};
+
+chrome.tabs.onActivated.addListener(async (tab) => {
+  if (tabObj[tab.tabId] === undefined) {
+    tabObj[tab.tabId] = {};
+  }
+  const [tabQuery] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  tabObj[tab.tabId] = {
+    ...tabObj[tab.tabId],
+    isComplete: urlStatus[tabQuery.status],
+    url: tabQuery.url,
+  };
+  chrome.tabs.sendMessage(tab.tabId, tabObj[tab.tabId]);
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, tab) => {
+  if (tabObj[tabId] === undefined) {
+    tabObj[tabId] = {};
+  }
+  if (tab.url) {
+    tabObj[tabId] = {
+      ...tabObj[tabId],
+      isComplete: urlStatus[tab.status],
+      url: tab.url,
+    };
+  }
+
+  if (tab.status === 'complete') {
+    tabObj[tabId] = { ...tabObj[tabId], isComplete: urlStatus[tab.status] };
+  }
+
+  chrome.tabs.sendMessage(tabId, tabObj[tabId]);
+  console.log(tabObj);
+});
+
+chrome.tabs.onDetached.addListener((tabId) => {
+  console.log('detached', tabId);
+});
