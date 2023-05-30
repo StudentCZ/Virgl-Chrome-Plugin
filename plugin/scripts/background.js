687bc1089259f0840eadce1c0024f488743c28b5
jekiquin <jci.dev.0@gmail.com>
2023-05-29 22:19:18 -0700

    added more todo comments

--- plugin/scripts/background.js
+++ plugin/scripts/background.js
@@ -4,6 +4,7 @@ chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
   console.log(sender)
   sendResponse('Saving job info')
   try {
+    // todo: token from localstorage will be sent to saveJobApplication
     await saveJobApplication(req)
     await chrome.tabs.sendMessage(sender.tab.id, 'Job info saved')
   } catch {
