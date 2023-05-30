496491d3450651fe3f9d00e8bd65da82e3d957ad
jekiquin <jci.dev.0@gmail.com>
2023-05-29 22:18:24 -0700

    added getuser functionality

--- plugin/scripts/utils/queries.js
+++ plugin/scripts/utils/queries.js
@@ -1,10 +1,14 @@
 const BASE_URL = 'http://localhost:3008'
 
 const ROUTES = {
-  JOB_DATA: '/jobData'
+  JOB_DATA: '/jobData',
+  GET_USER: '/user'
 }
 
 export const saveJobApplication = async (jobInfo) => {
+  const user = await getUser()
+  if (!user) throw new Error('Authentication failed')
+  jobInfo.userId = user.id
   return await fetch(`${BASE_URL}${ROUTES.JOB_DATA}`, {
     method: 'POST',
     headers: {
@@ -12,4 +16,10 @@ export const saveJobApplication = async (jobInfo) => {
     },
     body: JSON.stringify(jobInfo)
   })
+}
+
+// todo: getUser will have some token as a parameter for fetching user
+export const getUser = async () => {
+  return Promise.resolve({ id: 12345567 })
+  // return Promise.reject(null)
 }
